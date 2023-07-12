ce1b4be797674db5fc090eb754be47e8f7c7d6a9
kasunharshana352 <139376547+kasunharshana352@users.noreply.github.com>
2023-07-13 00:54:50 +0530

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 function Signup() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   const serverURL = process.env.REACT_APP_SERVER_URL;
 
